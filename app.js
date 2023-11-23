241dc7544a085eacd949b77199cf9b4c907a6988
Henry <[200~150098333+HenrydeJesusRamirez@users.noreply.github~>
2023-11-23 11:05:40 -0500

    Inicio repositorio JS HTML DOM

---
+++ app.js
@@ -0,0 +1,8 @@
+/*const acronimo = document.getElementsByTagName("span");//Obtener las primeras letras del subtitulo. 
+document.getElementsByTagName("h1").addEventListener("click", function(){
+  alert("This is an alert box");
+})*/
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "This is a new message";
+}
